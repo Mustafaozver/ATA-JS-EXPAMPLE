3b3f013e1b85618c17808d0f8a09273129cb0714
Mustafaozver <mustafa.ozver@hotmail.com>
2024-06-14 14:38:33 +0300

    1718365112 - SHPUSH (EXPRESS) - 14:38 - 14_06_2024

--- Controller/Express.js
+++ Controller/Express.js
@@ -12,7 +12,7 @@
 		//const userAgent = "" + req.headers["user-agent"];
 		
 		//res.setHeader("Access-Control-Allow-Origin", "http://" + ATA.config.DOMAIN + ":" + port + "/");
-		res.setHeader("Access-Control-Allow-Origin", domain);
+		//res.setHeader("Access-Control-Allow-Origin", domain);
 		res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 		//res.setHeader("Allow", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 		res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
