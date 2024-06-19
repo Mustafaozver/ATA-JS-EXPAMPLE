5ca3833339e9c8eebf723352e853b43b37bb613c
Mustafaozver <mustafa.ozver@hotmail.com>
2024-06-20 01:06:47 +0300

    1718834807 - SHPUSH (EXPRESS, i18n yapılacak) - 01:06 - 20_06_2024

--- Controller/Socket/Router/0.js
+++ Controller/Socket/Router/0.js
@@ -1,8 +1,8 @@
 module.exports=((ATA)=>{
 	
 	return (socketid, data)=>{
-		console.log("LOW", data);
+		//console.log("LOW", data);
 		//socket.emit("EXEC", "console.log(1453);");
-		ATA.Socket.IO.to(socketid).emit("EXEC", "console.log(1453);");
+		ATA.Socket.IO.to(socketid).emit("EXEC", "console.log(1682972);");
 	};
 })(ATA());
