c6c0f18b94dac940dc4cb0d3dd24d6c8b2992c8b
Mustafaozver <mustafa.ozver@hotmail.com>
2024-06-22 17:50:24 +0300

    1719067824 - SHPUSH (Mail yapılacak) - 17:50 - 22_06_2024

--- Controller/Mail.js
+++ Controller/Mail.js
@@ -1,7 +1,67 @@
-((ATA)=>{
-	const Mail = ANA.Library.Mail;
+((ATA) => {
+	const { CreateTransporter } = ANA.Library.Mail;
+	const { CompileEJSFile } = ANA.Library.Render;
+	const { C } = ANA.Library.i18n;
+	let Transporter = null;
+	
+	const config = ANA.Configurations.GetConstant("Mail");
+	const Environment = ANA.Configurations.GetConstant("Environment");
+	
+	const GenerateMailConfig = (option)=>{
+		return Object.assign({
+			from: config.MAIL,
+			to: "",
+			subject: "Deneme",
+			html: "",
+		}, {...option});
+	};
+	
+	const Send = async(to, subject, html)=>{
+		const mail = GenerateMailConfig({
+			to,
+			subject,
+			html
+		});
+		const promise = new Promise((resolve, reject)=>{
+			Transporter.sendMail(mail, (err, info)=>{
+				if(err)return reject(err);
+				resolve(info.response);
+			});
+		});
+		return await promise;
+	};
+	
+	const SendEJS = async(to, subject, filepath, render_data)=>{
+		const promise = new Promise((resolve, reject)=>{
+			CompileEJSFile(filepath, {
+				C,
+				ATA,
+				Environment,
+				...render_data,
+			}).then((data)=>{
+				Send(to, subject, data).then((data)=>{
+					resolve(data);
+				}).catch((err)=>{
+					reject(err);
+				});
+			}).catch((err)=>{
+				reject(err);
+			});
+		});
+		return await promise;
+	};
 	
 	ATA.Setups.push(()=>{
-		//
+		Transporter = CreateTransporter({
+			auth: {
+				user: config.USER,
+				pass: config.PASS,
+			},
+		});
 	});
+	
+	ATA.Mail = {
+		Send,
+		SendEJS,
+	};
 })(ATA());
--- Library/Mail.js
+++ Library/Mail.js
@@ -1,9 +1,24 @@
 module.exports=((ATA)=>{
 	const NM = ATA.Require("nodemailer");
 	
+	const CreateTransporter = (option)=>{
+		return NM.createTransport(Object.assign({
+			service: "gmail",
+			Host: "smtp.gmail.com",
+			port: 465,
+			secure: true,
+			logger: false,
+			debug: false,
+			secureConnection: false,
+			tls: {
+				rejectUnAuthorized: true
+			},
+		}, {...option}));
+	};
 	
 	return{
 		//...
+		CreateTransporter,
 	};
 	
 })(ATA());
