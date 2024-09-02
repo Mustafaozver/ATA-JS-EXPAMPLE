ca794e2f647054b94fb6973bb28b9f2edc65795e
Mustafaozver <mustafa.ozver@hotmail.com>
2024-09-02 21:12:35 +0300

    1725300755 - SHPUSH () - 21:12 - 02_09_2024

--- Controller/Express/Router/View.js
+++ Controller/Express/Router/View.js
@@ -4,7 +4,7 @@ module.exports=((ATA)=>{
 	const router = Router();
 	
 	app.set("views", ATA.Path.join(ATA.CWD, "./View/www/"));
-	router.use(static(ATA.Path.join(ATA.CWD, "./View/www/"), { index: "wushu.html" }));
+	router.use(static(ATA.Path.join(ATA.CWD, "./View/www/"), { index: "index.html" }));
 	router.use(static(ATA.Path.join(ATA.CWD, "./node_modules/"), { index: "package.json" }));
 	
 	//router.use(ATA.Require("./Controller/Express/Error/404.js"));
